fec4c7528b8f94636a28e1eb029563f501b047e3
VictorTamayoBriones <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-03-24 21:39:34 -0600

    new key

--- src/hooks/useGetWeather.jsx
+++ src/hooks/useGetWeather.jsx
@@ -1,23 +1,22 @@
 import axios from "axios"
 import { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../context/userContext';
-import { getWeather } from '../services/getWeather';
 
 export const useGetWeather =  () =>{
 
     const { userRandom } = useContext(UserContext);
+    const apiKey = 'dc5682dc88229268653b08b1d1fd5e70';
+    const city = userRandom.locationWeather;
 
     const[weather, setWeather]=useState({})
     const[formatWeather, setFormatWeather]=useState({});
 
-    const apiKey = 'dc5682dc88229268653b08b1d1fd5e70';
-    const city = userRandom.locationWeather;
     
     const getWeather = async () =>{
         try{
             const res = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
             setWeather(res.data)
-
+            
             setFormatWeather({
                 cityName: weather.name,
                 temp: weather.main.temp,
@@ -32,6 +31,11 @@ export const useGetWeather =  () =>{
         }
     }
     
+    useEffect(()=>{
+        getWeather()
+        console.log(formatWeather)
+    }, [city, userRandom])
+
     return {formatWeather, getWeather}
 }
 
